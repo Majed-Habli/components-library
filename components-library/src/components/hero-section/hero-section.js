a120ccf632b24c4fbd0bfdd7722b71ddcb6ec130
Majed-Habli <majedhabli2020@gmail.com>
2024-10-25 23:23:24 +0300

    Updated the hero section class asignment.

--- components-library/src/components/hero-section/hero-section.js
+++ components-library/src/components/hero-section/hero-section.js
@@ -2,7 +2,7 @@ import  "./hero-section.scss";
 const heroSection = () => {
     return (
         <div className="banner-text">
-            <p className="banner-title">Build sites faster with copy-ready <span>components</span></p>
+            <p className="banner-title">Build sites faster with copy-ready <span className="text-span-blue-300">components</span></p>
             <p className="banner-description">Build faster with accessible, copy-ready components designed for flexibility and usability. Focus on creating great experiences, not repetitive coding.</p>
         </div>
     )
