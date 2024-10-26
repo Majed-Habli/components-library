33dc9ed875e5307896a46e04ab2092e1be6ba47f
Majed-Habli <majedhabli2020@gmail.com>
2024-10-26 04:05:42 +0300

    Created a button component and the initial props.

---
+++ components-library/src/components/button/button.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classNames from 'classnames';
+import './button.scss';
+
+const Button = ({text, mode, size, theme}) => {
+
+    const buttonClass = classNames('button', {
+        [`btn--${mode}`]: mode,
+        [`btn--${size}`]: size,
+        [`btn--${theme}`]: theme,
+      });
+      
+    return (
+        <button className={buttonClass}>
+            {text}
+        </button>
+    )
+}
+
+export default Button;
