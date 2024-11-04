2dd9119363a571847dea073138feadeb3b7359d0
Majed-Habli <majedhabli2020@gmail.com>
2024-11-05 00:25:42 +0200

    Added disabled and arialabel props to the link button component and updated the structure.

--- components-library/src/components/link-button/link-button.js
+++ components-library/src/components/link-button/link-button.js
@@ -1,20 +1,27 @@
 import React from "react";
-import classNames from 'classnames';
-import '../button/button.scss';
+import classNames from "classnames";
+import "../button/button.scss";
 
-const LinkButton = ({text, mode, size, theme, href}) => {
-    
-    const buttonClass = classNames('button', {
-        [`btn--${mode}`]: mode,
-        [`btn--${size}`]: size,
-        [`btn--${theme}`]: theme,
-        [`hover--${theme}`]: theme,
-        [`link`]: href,
-      });
+const LinkButton = ({ text, mode, size, theme, href, ariaLabel, disabled }) => {
+  const buttonClass = classNames("button", {
+    [`btn--${mode}`]: mode,
+    [`btn--${size}`]: size,
+    [`btn--${theme}`]: theme,
+    [`hover--${theme}`]: theme,
+    [`link`]: href,
+  });
 
-    return (
-        <a className={buttonClass} href={href || "#"} tabIndex="0">{text}</a>
-    )
-}
+  return (
+    <a
+      className={buttonClass}
+      href={disabled ? undefined : href}
+      aria-label={ariaLabel}
+      aria-disabled={disabled}
+      tabIndex={disabled ? -1 : 0}
+    >
+      {text}
+    </a>
+  );
+};
 
 export default LinkButton;
