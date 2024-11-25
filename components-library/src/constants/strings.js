2804b505cf3b1870ab59b7436ab23ecb029fa19f
Majed-Habli <majedhabli2020@gmail.com>
2024-11-25 22:09:40 +0200

    Added the metaData for the widgets.

---
+++ components-library/src/constants/strings.js
@@ -0,0 +1,21 @@
+import Button from "assets/Button.svg";
+import Checkbox from "assets/Checkbox-icon.svg";
+import tib from "assets/text-Icon-Button.svg";
+
+export const WidgetsMetaData = [
+    {
+      label:"Button",
+      icon: Button,
+      description: "Use buttons to allow users o take action within your site or app.",
+    },
+    {
+      label:"Text-Icon button",
+      icon: tib,
+      description: "Jacob",
+    },
+    {
+      label:"Checkbox",
+      icon: Checkbox,
+      description: "Jacob",
+    },
+  ];
