4a557d926b968957aa33960a93a76398bf8e7dd2
Majed-Habli <majedhabli2020@gmail.com>
2024-11-25 22:24:31 +0200

    Updated the logic to sort based on search field.

--- components-library/src/components/navigation/sidebar.jsx
+++ components-library/src/components/navigation/sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchField from "components/search-field/search-field";
 import "./sidebar.scss";
 import WidgetList from "components/widget/widget";
@@ -6,11 +6,16 @@ import {WidgetsMetaData} from "../../constants/strings";
 
 
 const Sidebar = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const filteredWidgets = WidgetsMetaData.filter((widget) =>
+        widget.label.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+
     return (
         <div className="main-sidebar">
             {/* add the buttons for sorting the search bar layout */}
-            <SearchField/>
-            <WidgetList widgets={WidgetsMetaData}/>
+            <SearchField onSearch={(value) => setSearchTerm(value)}/>
+            <WidgetList widgets={filteredWidgets}/>
         </div>
     )
 }
