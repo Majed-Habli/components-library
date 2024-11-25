dec170e05cb5ec8491a6a0d44a827a9b9762d2db
Majed-Habli <majedhabli2020@gmail.com>
2024-11-25 22:08:22 +0200

    Updated the searchfield label.

--- components-library/src/components/search-field/search-field.js
+++ components-library/src/components/search-field/search-field.js
@@ -4,8 +4,8 @@ import "./search-field.scss";
 const SearchField = () => {
     return (
         <div className="form-group">
-            <input type="text" id="name" placeholder="search"></input>
-            <label for="name">Name</label>
+            <input type="text" id="search" placeholder="search"></input>
+            <label for="search">Search</label>
         </div>
     )
 }
