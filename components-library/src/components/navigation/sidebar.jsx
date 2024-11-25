8e118a34dfa90098dfc6b5c46eb6384a7ff17837
Majed-Habli <majedhabli2020@gmail.com>
2024-11-25 22:08:49 +0200

    Imported the widget list component and data array.

--- components-library/src/components/navigation/sidebar.jsx
+++ components-library/src/components/navigation/sidebar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import SearchField from "components/search-field/search-field";
 import "./sidebar.scss";
+import WidgetList from "components/widget/widget";
+import {WidgetsMetaData} from "../../constants/strings";
+
 
 const Sidebar = () => {
     return (
         <div className="main-sidebar">
             {/* add the buttons for sorting the search bar layout */}
             <SearchField/>
-            {/* components view */}
+            <WidgetList widgets={WidgetsMetaData}/>
         </div>
     )
 }
