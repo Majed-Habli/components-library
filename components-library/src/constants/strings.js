88c4feb4f94aa70879ab5fa692c02f72d0605c1f
Majed-Habli <majedhabli2020@gmail.com>
2024-12-02 23:52:13 +0200

    Updated the imports for the strings file adding a preview.

--- components-library/src/constants/strings.js
+++ components-library/src/constants/strings.js
@@ -1,12 +1,20 @@
-import Button from "assets/Button.svg";
+import ButtonImage from "assets/Button.svg";
 import Checkbox from "assets/Checkbox-icon.svg";
 import tib from "assets/text-Icon-Button.svg";
+import Button from "../components/button/button";
 
 export const WidgetsMetaData = [
     {
       label:"Button",
-      icon: Button,
-      description: "Use buttons to allow users o take action within your site or app.",
+      icon: ButtonImage,
+      description: "Use buttons to allow users to take action within your site or app.",
+      preview: (
+        <div className="preview-container">
+            <Button text="Discover" mode="solid" size="small" theme="green-700" />
+            <Button text="Discover" mode="solid" size="medium" theme="green-700" />
+            <Button text="Discover" mode="solid" size="large" theme="green-700" />
+        </div>
+    ),
     },
     {
       label:"Text-Icon button",
