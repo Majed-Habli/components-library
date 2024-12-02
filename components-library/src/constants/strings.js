f1ae2d80beab5e03857b0492460afdec7a474d69
Majed-Habli <majedhabli2020@gmail.com>
2024-12-03 01:45:16 +0200

    Added the preview for the text icon component.

--- components-library/src/constants/strings.js
+++ components-library/src/constants/strings.js
@@ -2,6 +2,7 @@ import ButtonImage from "assets/Button.svg";
 import Checkbox from "assets/Checkbox-icon.svg";
 import tib from "assets/text-Icon-Button.svg";
 import Button from "../components/button/button";
+import TextIconButton from "../components/text-icon-button/text-icon-button";
 
 export const WidgetsMetaData = [
     {
@@ -10,16 +11,26 @@ export const WidgetsMetaData = [
       description: "Use buttons to allow users to take action within your site or app.",
       preview: (
         <div className="preview-container">
+          <div className="row-preview">
             <Button text="Button" mode="solid" size="small" theme="blue-400" />
             <Button text="Button" mode="solid" size="medium" theme="blue-400" />
             <Button text="Button" mode="solid" size="large" theme="blue-400" />
+          </div>
         </div>
-    ),
+      ),
     },
     {
       label:"Text-Icon button",
       icon: tib,
       description: "Combine text and icons for clearer actions.",
+      preview: (
+        <div className="preview-container">
+          <div className="column-preview">
+            <TextIconButton leadingIcon="ArrowDownTrayIcon" text="Download" size="medium"  mode="solid" theme="blue-400"/>
+            <TextIconButton trailingIcon="ArrowDownTrayIcon" text="Download" size="medium"  mode="solid" theme="blue-400"/>
+          </div>
+        </div>
+      ),
     },
     {
       label:"Checkbox",
