462102d75370aff51d60af57f7bf497ee333fc71
Majed-Habli <majedhabli2020@gmail.com>
2024-12-10 00:42:53 +0200

    Updated the checkbox component structure.

--- components-library/src/components/checkbox/checkbox.js
+++ components-library/src/components/checkbox/checkbox.js
@@ -1,16 +1,21 @@
 import React from "react";
+import "./checkbox.scss";
 
 const CheckBox = ({ id, name, label, checked, onChange }) => {
     return (
-        <div>
+        <div className="checkbox-container">
             <input
                 type="checkbox"
                 id={id}
                 name={name}
+                className="checkbox-input"
                 checked={checked}
                 onChange={onChange}
             />
-            <label htmlFor={id}>{label}</label>
+            <label htmlFor={id} className="checkbox-label">
+                <span className="checkbox-custom"></span>
+                {label}
+            </label>
         </div>
     );
 };
