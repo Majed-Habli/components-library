8919a9c1b8db3d18fe3d0b6d175b81ff6c27d4ff
Majed-Habli <majedhabli2020@gmail.com>
2024-12-10 02:41:46 +0200

    Updated the props in the component preview.

--- components-library/src/components/checkbox/checkbox-preview.js
+++ components-library/src/components/checkbox/checkbox-preview.js
@@ -2,25 +2,19 @@ import React, { useState } from "react";
 import CheckBox from "./checkbox";
 
 const CheckBoxPreview = () => {
-    const [checked1, setChecked1] = useState(true);
-    const [checked2, setChecked2] = useState(false);
+    const [checked, setChecked] = useState(true);
 
     return (
         <div className="preview-container">
             <div className="column-preview">
                 <CheckBox
-                    id="preview1"
-                    name="preview1"
-                    label="Option 1"
-                    checked={checked1}
-                    onChange={(e) => setChecked1(e.target.checked)}
-                />
-                <CheckBox
                     id="preview2"
                     name="preview2"
-                    label="Option 2"
-                    checked={checked2}
-                    onChange={(e) => setChecked2(e.target.checked)}
+                    label="Checkbox"
+                    size="medium"
+                    theme="blue-500"
+                    checked={checked}
+                    onChange={(e) => setChecked(e.target.checked)}
                 />
             </div>
         </div>
