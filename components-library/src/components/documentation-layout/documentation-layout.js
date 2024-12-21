0e9b14872a3a2eb8a10be859ae70bf5e1d13744a
Majed-Habli <majedhabli2020@gmail.com>
2024-12-21 20:11:18 +0200

    Updated the domentation layout component.

--- components-library/src/components/documentation-layout/documentation-layout.js
+++ components-library/src/components/documentation-layout/documentation-layout.js
@@ -1,19 +1,21 @@
 import React from "react";
-// import "./documentation-layout.scss";
+import DocumentationSection from "./documentation-section";
+import "./documentation-layout.scss";
 
 const DocumentationLayout = ({ component }) => {
   return (
     <div className="documentation-layout">
-      <h1>{component.label}</h1>
-      <p>{component.description}</p>
-      <div className="preview">
-        <h2>Component Preview</h2>
-        {component.preview}
-      </div>
-      <div className="code-preview">
-        <h2>Code Example</h2>
-        <pre>{component.code}</pre>
-      </div>
+      <h1>{component.title}</h1>
+      <p>{component.titleDescription}</p>
+      {component.sections.map((section, index) => (
+        <DocumentationSection
+          key={index}
+          title={section.title}
+          descriptionBefore={section.descriptionBefore}
+          listItems={section.listItems}
+          descriptionAfter={section.descriptionAfter}
+        />
+      ))}
     </div>
   );
 };
