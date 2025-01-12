59dc800b4f2e79043b10ee1e2089fd8b7f608f76
Majed-Habli <majedhabli2020@gmail.com>
2025-01-13 00:34:51 +0200

    Updated the leading icon and trailing component logic and syntax.

--- components-library/src/components/accordion/accordion.js
+++ components-library/src/components/accordion/accordion.js
@@ -2,31 +2,41 @@ import React, { useState, useRef } from "react";
 import "./accordion.scss";
 import * as Icons from '@heroicons/react/24/solid';
 
-const Accordion = ({index, iconName}) => {
+const Accordion = ({index, trailingIcon, leadingIcon}) => {
   const [activeLeftIndex, setActiveLeftIndex] = useState(null);
 
   const panelRefs = useRef([]);
-  const IconComponent = Icons[iconName];
+  const TrailingIconComponent = Icons[trailingIcon];
+  const LeadingIconComponent = Icons[leadingIcon]
 
   const toggleAccordion = (index) => {
       setActiveLeftIndex(activeLeftIndex === index ? null : index);
     }
-    console.log(Icons);
+  
   return (
     <div key={index} className="accordion-container">
       <button
         className={`accordion ${activeLeftIndex === index ? "active" : ""}`}
         onClick={() => toggleAccordion(index)}
+        aria-expanded={activeLeftIndex === index}
+        aria-controls={`panel-${index}`}
       >
-        <span className={`icon-${iconName}`} aria-hidden="true">
-          <img src={`./${iconName}.svg`} alt={`${iconName} icon`} />
-        </span>
+        {leadingIcon && LeadingIconComponent && (
+          <span className={`icon-${leadingIcon}`} aria-hidden="true">
+            <LeadingIconComponent aria-hidden="true" />
+          </span>
+        )}
+
         <span className="button-text">Statement</span>
-        <span className="chevron-arrow-icon" >
-          <IconComponent aria-hidden="true"/>
-        </span>
+
+        {trailingIcon && TrailingIconComponent && (
+          <span className="chevron-arrow-icon" >
+            <TrailingIconComponent aria-hidden="true"/>
+          </span>
+        )}
       </button>
       <div
+        id={`panel-${index}`}
         ref={(el) => (panelRefs.current[index] = el)}
         className="panel"
         style={{
